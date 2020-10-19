49b94269ea065d6ae76b3fbe40c7e7b59ab064c1
wangkt <wangkt@yiche.com>
2020-10-19 19:52:03 +0800

    ok 了基本环境

--- src/main/java/com/yc/jenkins/helper/DemoApplication.java
+++ src/main/java/com/yc/jenkins/helper/DemoApplication.java
@@ -1,16 +1,21 @@
-package com.ktwang.demo;
+package com.yc.jenkins.helper;
 
+import com.yc.jenkins.helper.entity.po.BrandClueAdvertise;
+import com.yc.jenkins.helper.mapper.BrandClueAdvertiseMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import java.util.Date;
 
 @SpringBootApplication
 @Controller
 public class DemoApplication {
+	@Resource
+	BrandClueAdvertiseMapper brandClueAdvertiseMapper;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -20,6 +25,7 @@ public class DemoApplication {
 	@RequestMapping("/")
 	@ResponseBody
 	public String index(){
+		BrandClueAdvertise brandClueAdvertise =brandClueAdvertiseMapper.selectByPrimaryKey(1L);
 		return "hello world  " + new Date();
 	}
 }
